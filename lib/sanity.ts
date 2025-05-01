b65f44f7e160b3986f76ac41d4cb7b7fbe6ae38d
Bee-code-tech <babawaleolatunji64@gmail.com>
2025-05-01 01:26:16 +0100

    Add API version to Sanity client configuration

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -2,6 +2,7 @@ import { createClient } from '@sanity/client';
 
 export const client = createClient({
   dataset: 'production',
-  projectId: '5ahtvdeq',
+    projectId: '5ahtvdeq',
+  apiVersion: '3.87.0',
   useCdn: false,
 });
