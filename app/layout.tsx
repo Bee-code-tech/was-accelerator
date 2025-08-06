9ebdea73b6893152d9b13262405715f4ff8a3013
Bee-code-tech <babawaleolatunji64@gmail.com>
2025-08-06 16:35:07 +0100

    pixel update

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,18 +58,18 @@ export default function RootLayout({
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1029885945276073');
+          fbq('init', '1814140616148616');
           fbq('track', 'PageView');
         `,
       }}
     ></script>
     <noscript>
-  <img
-    height="1"
-    width="1"
-    style={{ display: 'none' }}
-    src="https://www.facebook.com/tr?id=1029885945276073&ev=PageView&noscript=1"
-  />
+          <img
+            height="1"
+            width="1"
+            
+src="https://www.facebook.com/tr?id=1814140616148616&ev=PageView&noscript=1"
+/>
 </noscript>
 
       </head>
@@ -82,3 +82,4 @@ export default function RootLayout({
     </html>
   )
 }
+
